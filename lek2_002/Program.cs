2b416e1eef3f308cb627eb2c9cf5a198c1eb0b07
Daria <dashavolodina@gmail.com>
2023-01-22 21:49:40 +0800

    Поиск мах в массиве

---
+++ lek2_002/Program.cs
@@ -0,0 +1,32 @@
+﻿// Нахождения максимума из 9 массива
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+
+}
+
+void PrintArray(int[] col)
+{
+    int len = col.Length;
+    int position = 0;
+    while (position < len)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+
+}
+
+int[] arr = new int[9];
+
+FillArray(arr);
+PrintArray(arr);
+
+// Console.WriteLine(max);
