a78e3dd9503b5de2e555fde965eb35fa36c295d4
Daria <dashavolodina@gmail.com>
2023-01-22 22:09:11 +0800

    создание, заполнение массива, поиск числа в нем

--- lek2_002/Program.cs
+++ lek2_002/Program.cs
@@ -24,9 +24,30 @@ void PrintArray(int[] col)
 
 }
 
+int IndexOf(int[] array, int find)
+{
+    int lengt = array.Length;
+    int index = 0;
+    int positi = -1;
+    while (index < lengt)
+    {
+        if (array[index] == find)
+        {
+            positi = index; 
+            break;
+        }
+       
+        index++;
+    }
+    return positi;
+}
+
 int[] arr = new int[9];
 
 FillArray(arr);
 PrintArray(arr);
+Console.WriteLine();
+int pos = IndexOf(arr, 4);
+Console.WriteLine(pos);
 
 // Console.WriteLine(max);
